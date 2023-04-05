6928362883aff4d95a25663fc01ea8174a279515
BinkaArushankina <albinarushankina@gmail.com>
2023-04-05 09:43:47 +0200

    algorithms

--- src/Homework/Homework.java
+++ src/Homework/Homework.java
@@ -8,5 +8,5 @@ public class Homework {
 //f(n) = 235                 O(1)
 //f(n) = n^2 + 7n +123       O(n^2)
 //f(n) = n^3 +2000n +8765    O(n^3)
-//f(n) = n + sqrt(n)         O(log n)
+//f(n) = n + sqrt(n)         O(n)
 }
